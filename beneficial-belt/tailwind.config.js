15f0401c97fb8ecef59264eb70057cb94621163a
Cristian MeSu <cristian.mendoza.s@hotmail.com>
2025-11-03 21:10:29 -0500

    fixiing error on tailwind configuration

---
+++ beneficial-belt/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1025px',
+      'xl': '1600px',
+    },
+    extend: {colors: {
+      'primary-color': 'var(--primary-color)',
+      'secondary-color': 'var(--secondary-color)',
+      'redSocial-color': 'var(--redSocial-color)',
+      'darkblue-color': 'var(--darkblue-color)',
+    },},
+    fontFamily: {
+      lindsey: ['"lindsey-signature"', 'sans-serif'],
+      alverata: ['"alverata"', 'sans-serif'],
+      bebas: ['"bebas"', 'sans-serif'],
+      bebasBold: ['"bebas-bold"', 'sans-serif'],
+      lato: ['"lato"', 'sans-serif'],
+      latoBold: ['"lato-bold"', 'sans-serif'],
+    },
+  },
+
+  plugins: [],
+};
